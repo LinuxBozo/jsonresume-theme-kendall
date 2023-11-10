8b2312b17d84fe83596e19f22c5f40e0042e6642
Grigorii Bushmanov <Grigorii@Bushmanov.online>
2023-11-10 23:53:49 +0300

    template redesign

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ function render(resumeObject) {
 
     resumeObject.css = fs.readFileSync(__dirname + "/style.css", "utf-8");
     resumeObject.printcss = fs.readFileSync(__dirname + "/print.css", "utf-8");
-    var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
+    var theme = fs.readFileSync(__dirname + '/resume.template.html', 'utf8');
     var resumeHTML = Mustache.render(theme, resumeObject);
 
 
